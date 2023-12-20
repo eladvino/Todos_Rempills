a1bef2290f025591254d1f64b21639acbe2879df
eladv <elad.vino1@gmail.com>
2023-12-20 20:31:36 +0200

    Elad

---
+++ app/src/main/java/com/example/todos_rempills/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.todos_rempills;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
